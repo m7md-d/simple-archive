500f6b2d22a3b55b5af587e4b22caf88ab25634f
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-31 14:18:43 +0300

    Add enum definition for command flags in flags.h

--- flags.h
+++ flags.h
@@ -1,6 +1,16 @@
 #ifndef FLAGS_H
 #define FLAGS_H
 
+typedef enum
+{
+    HELP = 'h',
+    CREATE = 'c',
+    INSERT = 'i',
+    DELETE = 'd',
+    EXTRACT = 'e',
+    LIST = 'l'
+} flag;
+
 void flag_check(int, char **);
 
 #endif /* FLAGS_H */
