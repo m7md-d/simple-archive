9fb1dc90cd1fcb96d9fe73e5d065aa8a09f4afae
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-31 14:12:30 +0300

    Implement arc functions in arc.c and update arc.h with delete_arc declaration

---
+++ arc.c
@@ -0,0 +1,26 @@
+#include "arc.h"
+
+
+
+arc *creat_arc(void)
+{}
+
+
+void insert_file(arc *arc_name, char *filename)
+{}
+
+
+void delete_file(arc *arc_name, char *filename)
+{}
+
+
+void extract_file(arc *arc_name, char *filename)
+{}
+
+
+void list_files(arc *arc_name)
+{}
+
+
+void delete_arc(arc *arc_name)
+{}
