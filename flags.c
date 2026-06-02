3cd69eac2048d7b3ef3fddbfd91c6749d6593779
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-06-02 13:52:38 +0300

    Update usage messages in print_help and flag_check functions for improved clarity and consistency

--- flags.c
+++ flags.c
@@ -6,13 +6,15 @@
 /* print usage information listing all options */
 void print_help(void)
 {
-    printf("usage: archon [-c|-i|-d|-e|-l|-h] <archive> [file]\n\n");
-    printf("  -c <archive>          create archive\n");
-    printf("  -i <archive> <file>   insert file\n");
-    printf("  -d <archive> <file>   delete file\n");
-    printf("  -e <archive> <file>   extract file\n");
-    printf("  -l <archive>          list contents\n");
-    printf("  -h                    this help\n");
+    printf("usage: archon [-c|-i|-d|-e|-l|-n|-r|-h] <archive> [file]\n\n");
+    printf("  -c <archive>                         create archive\n");
+    printf("  -i <archive> <file>                  insert file\n");
+    printf("  -d <archive> <file>                  delete file\n");
+    printf("  -x <archive> <file>                  extract file\n");
+    printf("  -l <archive>                         list contents\n");
+    printf("  -n <archive>                         number of files\n");
+    printf("  -r <archive> <old_file> <new_file>   replace file\n");
+    printf("  -h                                   this help\n");
 }
 
 /* parse command-line flags and dispatch to the matching arc operation */
@@ -33,7 +35,7 @@ void flag_check(int argc, char **argv)
 
     case CREATE:
         if (argc < 3) {
-            fprintf(stderr, "usage: archon -c <archive>\n");
+            fprintf(stderr, "usage: <%s> -c <archive>\n", *argv);
             return;
         }
         creat_arc(argv[2]);
@@ -41,7 +43,7 @@ void flag_check(int argc, char **argv)
 
     case INSERT:
         if (argc < 4) {
-            fprintf(stderr, "usage: archon -i <archive> <file>\n");
+            fprintf(stderr, "usage: <%s> -i <archive> <file>\n", *argv);
             return;
         }
         insert_file(argv[2], argv[3]);
@@ -49,7 +51,7 @@ void flag_check(int argc, char **argv)
 
     case DELETE:
         if (argc < 4) {
-            fprintf(stderr, "usage: archon -d <archive> <file>\n");
+            fprintf(stderr, "usage: <%s> -d <archive> <file>\n", *argv);
             return;
         }
         delete_file(argv[2], argv[3]);
@@ -57,7 +59,7 @@ void flag_check(int argc, char **argv)
 
     case EXTRACT:
         if (argc < 4) {
-            fprintf(stderr, "usage: archon -e <archive> <file>\n");
+            fprintf(stderr, "usage: <%s> -e <archive> <file>\n", *argv);
             return;
         }
         extract_file(argv[2], argv[3]);
@@ -65,7 +67,23 @@ void flag_check(int argc, char **argv)
 
     case LIST:
         if (argc < 3) {
-            fprintf(stderr, "usage: archon -l <archive>\n");
+            fprintf(stderr, "usage: <%s> -l <archive>\n", *argv);
+            return;
+        }
+        list_files(argv[2]);
+        break;
+
+    case NFILES:
+        if (argc < 3) {
+            fprintf(stderr, "usage: <%s> -n <archive>\n", *argv);
+            return;
+        }
+        list_files(argv[2]);
+        break;
+
+    case REPLACE:
+        if (argc < 3) {
+            fprintf(stderr, "usage: <%s> -r <archive> <old_file> <new_file>\n", *argv);
             return;
         }
         list_files(argv[2]);
